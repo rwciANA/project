8cdf79644a2cc01f64ea35585195037141d776ec
rwciANA <rcondorii@unsa.edu.pe>
2026-07-23 23:48:31 -0500

    final

--- backend/veterinaria/settings.py
+++ backend/veterinaria/settings.py
@@ -4,6 +4,7 @@ Django settings for veterinaria project.
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,6 +89,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    
+    
 }
 
 
