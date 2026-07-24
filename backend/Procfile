391936eb13fe6affac0822ee54eaaab61490daa9
rwciANA <rcondorii@unsa.edu.pe>
2026-07-23 23:11:40 -0500

    hola

---
+++ backend/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn veterinaria.wsgi:application
--- backend/veterinaria/settings.py
+++ backend/veterinaria/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-
+from django.conf import settings
+from django.conf.urls.static import static
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,7 +78,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'veterinaria.wsgi.application'
+WSGI_APPLICATION = 'veterinaria.wsgi.app'
 
 
 # Database
